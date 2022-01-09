ef33f74f4f0f0fb2876189827acd9a111ccfc8b0
dev_ElderLab <enderlabo@gmail.com>
2022-01-09 15:05:21 -0500

    upload app

---
+++ Shared/EcommerceAppKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppKitApp.swift
+//  Shared
+//
+//  Created by Laborit on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
